7f23827c1cfd1719cd5187e46a4459f41b98f566
tvp <tvp@wherzat.com>
2021-04-01 15:56:48 +0700

    Setup install dir

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 cmake -G Ninja -Bbuild -DCMAKE_CXX_COMPILER="D:/msys64/mingw64/bin/c++.exe" -DCMAKE_BUILD_TYPE=Debug
 cmake --build ./build/ --target SGEyes --VERBOSE=1
+cmake --install ./build/ --prefix .
