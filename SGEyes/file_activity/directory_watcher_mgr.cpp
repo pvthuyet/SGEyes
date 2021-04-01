a54b7ceb6407798653489485700dc8c3a5a75ca1
tvp <tvp@wherzat.com>
2021-04-01 23:01:46 +0700

    Add vld for msvc

--- SGEyes/file_activity/directory_watcher_mgr.cpp
+++ SGEyes/file_activity/directory_watcher_mgr.cpp
@@ -6,8 +6,6 @@ SAIGON_NAMESPACE_BEGIN
 task_timer::status directory_watcher_mgr::on_timer()
 {
 	LOGENTER;
-	int* x = new int(1);
-	//*x = 2;
 	LOGEXIT;
 	return task_timer::status::CONTINUE;
 }
--- SGEyes/main.cpp
+++ SGEyes/main.cpp
@@ -3,12 +3,14 @@
 
 #include "the_app.hpp"
 #include "spdlog_header.h"
+#include "vld.h"
 
 int main()
 {
     namespace sg = saigon;
     // Initialize logger
     sg::initialze();
+    int* x = new int(1);
 
     // start app
     sg::the_app::get_instance().start();
